5b80eb464b05f18885f36ea3f9bbdd9dc0e12f1d
Aahan Madhok <151942445+aahan29@users.noreply.github.com>
2024-10-11 15:04:08 -0400

    Update SpringWeightCalculator.java

--- SpringWeightCalculator.java
+++ SpringWeightCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class asst1_madhokaa {
+public class SpringWeightCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
